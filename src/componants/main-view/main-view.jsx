d13dc5c5ff5715441196d322b3a8d19e552b5063
Mike <87212248+Ironmike84@users.noreply.github.com>
2022-01-21 16:41:22 -0500

    Updated
    New MainView

--- src/componants/main-view/main-view.jsx
+++ src/componants/main-view/main-view.jsx
@@ -1,40 +1,61 @@
 import React from 'react';
+import axios from 'axios';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 export class MainView extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            movies: [
-                { _id: 1, Title: 'Inception', Description: 'One of the Best Movies of the Decade!!', Genre: 'Drama', Rating: 'PG-13', ImagePath: 'https://m.media-amazon.com/images/M/MV5BZGFjOTRiYjgtYjEzMS00ZjQ2LTkzY2YtOGQ0NDI2NTVjOGFmXkEyXkFqcGdeQXVyNDQ5MDYzMTk@._V1_.jpg'},
-                { _id: 2, Title: 'The Shawshank Redemption', Description: 'A Really Sad Story But A Good Movie.', Genre: 'Drama', Rating: 'R', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg'},
-                { _id: 3, Title: 'Gladiator', Description: 'Yet another Sad story But a really Good Movie!!!', Genre: 'Drama', Rating: 'R', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg'}],
-            selectedMovie: null
-        };
-
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      selectedMovie: null,
+      user: null
+    };
+  }
+componentDidMount(){
+  axios.get('https://muvies-app.herokuapp.com/Movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
+
 setSelectedMovie(newSelectedMovie) {
     this.setState({
         selectedMovie: newSelectedMovie
     });
 }
+
+onLoggedIn(user) {
+  this.setState({
+    user
+  });
+}
 render() {
-    const { movies, selectedMovie } = this.state;
+  const { movies, selectedMovie, user } = this.state;
 
+  /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+  if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+  // Before the movies have been loaded
+  if (movies.length === 0) return <div className="main-view" />;
 
-    return (
-      <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-          ))
-        }
-      </div>
-    );
-  }
+  return (
+    <div className="main-view">
+      {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
+      {selectedMovie
+        ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+        : movies.map(movie => (
+          <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+       ))
+      }
+    </div>
+  );
+}
 
 }
 
